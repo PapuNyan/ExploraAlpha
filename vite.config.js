f846833df1e264a647a0e89e0f0c73888088faf1
2valdo5 <valdo25ch@gmail.com>
2026-05-26 14:50:27 -0600

    optimización de contenido multimedia by valdo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 75, // Comprime los JPG al 75% de calidad
+      },
+      png: {
+        quality: 75, // Comprime los PNG al 75% de calidad
+      },
+      webp: {
+        quality: 80, // Comprime los WebP al 80% de calidad
+      },
+      svg: {
+        multipass: true, // Optimiza el código interno de los SVG
+      },
+    })
+  ],
   optimizeDeps: {
     include: ['react-globe.gl', 'three']
   },
